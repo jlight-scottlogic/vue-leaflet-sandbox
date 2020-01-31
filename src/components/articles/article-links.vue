3af3a249b39f9a7db8196ad0c2f54bddbfddfefa
Jim Light <jlight@scottlogic.com>
2020-01-31 14:08:59 +0000

    emit click event from article links

--- src/components/articles/article-links.vue
+++ src/components/articles/article-links.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="article-links">
-        <div v-for="link in links" :key="link.code" class="article-link">{{link.name}}</div>
+        <div
+            v-for="link in links"
+            class="article-link"
+            :key="link.code"
+            @click.stop="handleClick(link)"
+        >{{link.name}}</div>
     </div>
 </template>
 
@@ -9,6 +14,11 @@ export default {
     name: 'article-links',
     props: {
         links: Array
+    },
+    methods: {
+        handleClick(link) {
+            this.$emit('linkclicked', link);
+        }
     },
     components: {}
 };
@@ -21,11 +31,16 @@ export default {
 .article-links > .article-link {
     display: inline-block;
     background-color: #42b983;
+    border: 2px solid #42b983;
     padding: 0.25rem 0.5rem;
     border-radius: 15px;
     font-size: 0.75rem;
     font-weight: bold;
     margin-left: 0.25rem;
+}
+.article-links > .article-link:hover {
+    border: 2px solid black;
+    cursor: pointer;
 }
 .article-links > .article-link:first-child {
     margin-left: 0;
